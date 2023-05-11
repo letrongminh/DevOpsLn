03d05859b376e39e4e0bb14003d93df435af21d9
letrongminh <minh.letrong.vn@gmail.com>
2023-05-12 02:53:01 +0300

    fix api

--- app/api/routes/user.py
+++ app/api/routes/user.py
@@ -21,7 +21,6 @@ user_collection = db_connect["user"]
     name="user:add-user",
 )
 
-# async def add_user_info(data_input: UserInfoInput = Depends(), file: UploadFile = File(...)):
 async def add_user_info(data_input: UserInfoInput = Depends()):
     if not data_input:
         raise HTTPException(
@@ -29,21 +28,15 @@ async def add_user_info(data_input: UserInfoInput = Depends()):
     try:
         file_path = None
         student_object = user_collection.find_one({"name": data_input.name, "year": data_input.year})
-        # if file:
-        #     file_path = os.path.join(MEDIA_PATH, file.filename)
-        #     with open(file_path, "wb") as f:
-        #         while contents := file.file.read(1024 * 1024):
-        #             f.write(contents)
+
         if student_object:
             logging.info("Student object exist")
         else:
             insert_result = user_collection.insert_one(
                 {
                     "name": data_input.name,
-                    # "program": data_input.program,
                     "year": data_input.year,
                     "sex": data_input.sex,
-                    # "avatar": file.filename,
                     "title": data_input.title,
                     "university": data_input.university
                 }
@@ -57,10 +50,8 @@ async def add_user_info(data_input: UserInfoInput = Depends()):
     print(student_object)
     return UserInfoResponse(
         name=student_object.get("name"),
-        # program=student_object.get("program"),
         year=student_object.get("year"),
         sex=student_object.get("sex"),
-        # avatar=student_object.get("avatar"),
         university=student_object.get("university"),
         title=student_object.get("title"),
     )
@@ -79,10 +70,8 @@ async def get_all_user():
             object_list.append(
                 {
                     "name": student_object.get("name"),
-                    # "program": student_object.get("program"),
                     "year": student_object.get("year"),
                     "sex": student_object.get("sex"),
-                    # "avatar": student_object.get("avatar"),
                     "university": student_object.get("university"),
                     "title": student_object.get("title"),
                 }
@@ -90,28 +79,3 @@ async def get_all_user():
         return AllUserInfoResponse(users=object_list)
     except Exception as err:
         raise HTTPException(status_code=500, detail=f"Exception: {err}")
-
-
-# @router.get(
-#     "/media/{avatar}",
-#     responses={
-#         200: {
-#             "description": "A picture of a vector image.",
-#             "content": {
-#                 "image/jpeg": {
-#                     "example": "No example available. Just imagine a picture of a vector image."
-#                 }
-#             },
-#         }
-#     },
-# )
-
-# def image_endpoint(avatar: str):
-#     file_path = os.path.join(MEDIA_PATH, avatar)
-#     if os.path.exists(file_path):
-#         return FileResponse(file_path, media_type="image/jpeg", filename=avatar)
-#     return {"error": "File not found!"}
-
-
-
-
